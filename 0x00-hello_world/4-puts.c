0cdae3f7102c6605d6814c4099e8df543071a7f9
Martin Kiprotich <martinkiprotich.mk@gmail.com>
2023-02-18 12:06:10 -0800

    Trial 3 of puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/* this is the main function
+ * to print out a string using
+ * puts function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
