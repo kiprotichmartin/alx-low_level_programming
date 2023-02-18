0ac3b0ccf38de510d2f9fb627b03dec7ba95c8da
Martin Kiprotich <martinkiprotich.mk@gmail.com>
2023-02-18 12:20:29 -0800

    Completed printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point
+ * @void: a void arguement or parameter
+ *
+ * Description: this is the main function for our
+ * C program with void or no arguments
+ * Return: the returned value is zero
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
